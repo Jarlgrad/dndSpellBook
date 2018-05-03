90075c8cb9b91e4e1f3e9c37f00792a2abf25bee
Timothy Jalgard <timothy.jalgard@academicwork.se>
2018-05-03 13:44:50 +0200

    initial commit: initial skeleton setup

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+console.log("Henlo dnd world!");
+
+ReactDOM.render(<App />, document.getElementById('root'));
