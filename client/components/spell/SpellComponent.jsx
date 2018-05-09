61f0ce037c0911f7d6b3237af4a160162bdea9f9
Timothy Boris Jalgard <timothy.jalgard@gmail.com>
2018-05-09 14:43:45 +0200

    trying to map out nested objects. part 1

--- client/components/spell/SpellComponent.jsx
+++ client/components/spell/SpellComponent.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
 const SpellComponent = ({spell}) => (
+    
 
     <div>
         <h4> Name: {spell.name} </h4>
         <ul>
             <li> Level required: {spell.level} </li>
+            <li> </li>
             <li> Range: {spell.range} </li>
             <li> {spell.desc} </li>
         </ul>
     </div>
 );
+
+let components = "W";
 
 export default SpellComponent;
--- client/components/spellbook/SpellBookComponent.jsx
+++ client/components/spellbook/SpellBookComponent.jsx
@@ -1,40 +1,41 @@
 import React from 'react';
-import { Spell, getSpell } from '../../services/spellservice.js';
+import { getSpell } from '../../services/spellservice.js';
 import SpellComponent from '../spell/SpellComponent.jsx';
+import Spell from '../../services/models/spell.js';
 
 export default class SpellBookComponent extends React.Component {
-    
     constructor(props){
         super(props)
         this.state = { spell: {} }
     }
 
     componentWillMount() {
-        getSpell()
+        getSpell().then(spell => new Spell(spell))
             .then(spell => {
-
                 this.setState({
                     spell: spell
                 });
-
-                return spell;
+                console.log(this.state);
             });
-            // .then(spell => spell.map(
-            //     name = spell.name
-            // ));
-
-        // console.log("after fetched", fetched) ;
-        // this.setState({
-        //     spell: fetched
-        // });
-
-    }
+    };
 
     render() {
+
+        const render = <SpellComponent spell={this.state.spell} />
+
+        let components = [];
+        if (this.state.spell.components){
+            console.log("this.state.spell is active", this.state);
+            components = this.state.spell.components
+               .map( (c, index) => <span key={index}>{c}, </span> );
+        }
+
         return (
             <div>
                 <span> Spell Book of lives </span>
-                <SpellComponent spell={this.state.spell} />
+                <div> { render } </div>
+                <div> Components: {components } </div>
+                {/* <SpellComponent spell={this.state.spell} /> */}
             </div>
         );
     }
